3429bf449928e22f96f9154bbc5ec0be03bdffbc
Mike Bowman <mbowman2@gmail.com>
2022-01-17 15:19:44 -0500

    Basic setup working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import Calculator from './components/Calculator';
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <>
+      <header>
+        <h1 className="text-2xl">Breadulator</h1>
+        <p>Easily convert to and from baker's percentages.</p>
+        <p>Scale recipes up and down with a click.</p>
+      </header>
+      <main>
+        <p>Body here</p>
+        <Calculator />
+      </main>
+      <footer className="text-center">
+        <p>&copy; Mike Bowman - 2022</p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
