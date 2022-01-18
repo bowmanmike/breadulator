6339ab87a6752be56dd5b948c4a68fc57514137c
Mike Bowman <mbowman2@gmail.com>
2022-01-17 19:38:15 -0500

    Temp comment

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,3 +1,57 @@
-const Calculator = () => <div>Calculator</div>;
+// import React, { useState } from 'react';
+
+// interface FormState {
+//   flour: number;
+//   water: number;
+//   salt: number;
+//   yeast: number;
+// }
+
+const Calculator = () => (
+  <>
+    <div>Calculator</div>
+    {/* <form onSubmit={submitForm}> */}
+    <form>
+      <label htmlFor="flour">Flour</label>
+      <input type="number" />
+    </form>
+  </>
+);
+// const [values, setValues] = useState({
+//   flour: 0,
+//   water: 0,
+//   salt: 0,
+//   yeast: 0,
+// } as FormState);
+
+// const resetForm = (): void => {
+//   const newValues: FormState = Object.keys(values).reduce(
+//     (acc, key: string) => {
+//       acc[key] = 0;
+//       return acc;
+//     },
+//     {}
+//   );
+
+//   setValues(newValues);
+// };
+
+// function updateValues(e: React.FormEvent<HTMLFormElement>) {
+//   let { value } = e.target;
+//   if (e.target.type === 'number') {
+//     value = parseInt(e.target.value);
+//   }
+
+//   setValues({
+//     ...values,
+//     [e.target.name]: value,
+//   });
+// }
+
+// function submitForm(e: React.FormEvent<HTMLFormElement>) {
+//   e.preventDefault();
+
+//   resetForm();
+// }
 
 export default Calculator;
