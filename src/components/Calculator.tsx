1d8576605a66a01b4d429ea119212fa740b01184
Mike Bowman <mbowman2@gmail.com>
2022-01-17 22:02:11 -0500

    Form wired up and typescript compiling

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,57 +1,80 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
-// interface FormState {
-//   flour: number;
-//   water: number;
-//   salt: number;
-//   yeast: number;
-// }
+interface FormState {
+  flour: number;
+  water: number;
+  salt: number;
+  yeast: number;
+}
 
-const Calculator = () => (
-  <>
-    <div>Calculator</div>
-    {/* <form onSubmit={submitForm}> */}
-    <form>
-      <label htmlFor="flour">Flour</label>
-      <input type="number" />
-    </form>
-  </>
-);
-// const [values, setValues] = useState({
-//   flour: 0,
-//   water: 0,
-//   salt: 0,
-//   yeast: 0,
-// } as FormState);
+const Calculator = () => {
+  const [values, setValues] = useState({
+    flour: 0,
+    water: 0,
+    salt: 0,
+    yeast: 0,
+  } as FormState);
 
-// const resetForm = (): void => {
-//   const newValues: FormState = Object.keys(values).reduce(
-//     (acc, key: string) => {
-//       acc[key] = 0;
-//       return acc;
-//     },
-//     {}
-//   );
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-//   setValues(newValues);
-// };
+    console.log('submitting');
+    console.log({ values });
+  };
 
-// function updateValues(e: React.FormEvent<HTMLFormElement>) {
-//   let { value } = e.target;
-//   if (e.target.type === 'number') {
-//     value = parseInt(e.target.value);
-//   }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = parseInt(e.target.value);
 
-//   setValues({
-//     ...values,
-//     [e.target.name]: value,
-//   });
-// }
+    setValues({
+      ...values,
+      [e.target.name]: val,
+    });
+  };
 
-// function submitForm(e: React.FormEvent<HTMLFormElement>) {
-//   e.preventDefault();
+  const resetForm = () => {
+    const newValues: FormState = Object.keys(values).reduce((acc, key) => {
+      acc[key as keyof typeof values] = 0;
+      return acc;
+    }, {} as FormState);
 
-//   resetForm();
-// }
+    setValues(newValues);
+  };
+
+  return (
+    <>
+      <div>Calculator</div>
+      <form onSubmit={submitForm}>
+        {Object.keys(values).map((field) => (
+          <div key={field}>
+            <label htmlFor={field}>
+              {field.replace(/^\w/, (c) => c.toUpperCase())}
+            </label>
+            <input
+              name={field}
+              type="number"
+              onChange={handleChange}
+              value={values[field as keyof typeof values]}
+            />
+          </div>
+        ))}
+        <button type="submit">Submit</button>
+        <button type="button" onClick={resetForm}>
+          Clear
+        </button>
+      </form>
+      <div>
+        <h1>Percentages</h1>
+      </div>
+      <div>
+        <h1>State</h1>
+        {Object.keys(values).map((k) => (
+          <div key={k}>
+            {k} - {values[k as keyof typeof values]}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Calculator;
