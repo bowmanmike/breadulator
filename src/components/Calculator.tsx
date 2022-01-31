245eba1dc7b2ea75c265ffbb9feb9952b16de4e1
Mike Bowman <mbowman2@gmail.com>
2022-01-31 16:16:39 -0500

    Work on swapping percentages for grams

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -15,6 +15,9 @@ const Calculator = () => {
 
   const [scale, setScale] = useState(1.0);
 
+  const [mode, setMode] = useState('WEIGHT' as CalcMode);
+  const swapMode = () => setMode((prevMode) => (prevMode === 'WEIGHT' ? 'PERCENTAGE' : 'WEIGHT'));
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const val = parseFloat(e.target.value);
 
@@ -25,19 +28,47 @@ const Calculator = () => {
     <div className="flex flex-col space-x-4 bg-green-500 md:flex-row">
       <div className="w-full border border-gray-400 md:w-1/2">
         <h2>Recipe</h2>
-        {Recipe.ingredients().map((ingredient) => (
-          <div key={`input-${ingredient}`}>
-            <label htmlFor={ingredient}>{ingredient.replace(/^\w/, (c) => c.toUpperCase())}</label>
-            <input
-              type="number"
-              min={0}
-              step="0.5"
-              value={recipe[ingredient]}
-              onChange={handleChange}
-              name={ingredient}
-            />
-          </div>
-        ))}
+        <form>
+          <label htmlFor="mode-weight">Weight</label>
+          <input type="radio" name="choose-mode" id="mode-weight" checked={mode === 'WEIGHT'} onChange={swapMode} />
+          <label htmlFor="mode-percentage">Percentage</label>
+          <input
+            type="radio"
+            name="choose-mode"
+            id="mode-percentage"
+            checked={mode === 'PERCENTAGE'}
+            onChange={swapMode}
+          />
+        </form>
+        {mode === 'WEIGHT' &&
+          Recipe.ingredients().map((ingredient) => (
+            <div key={`input-${ingredient}`}>
+              <label htmlFor={ingredient}>{ingredient.replace(/^\w/, (c) => c.toUpperCase())}</label>
+              <input
+                type="number"
+                min={0}
+                step="0.5"
+                value={recipe[ingredient]}
+                onChange={handleChange}
+                name={ingredient}
+              />
+            </div>
+          ))}
+        {mode === 'PERCENTAGE' &&
+          Recipe.ingredients().map((ingredient) => (
+            <div key={`input-${ingredient}`}>
+              <label htmlFor={ingredient}>{ingredient.replace(/^\w/, (c) => c.toUpperCase())}</label>
+              <input
+                type="number"
+                min={0}
+                step="0.5"
+                value={recipe.bakersPercentage(ingredient)}
+                onChange={handleChange}
+                name={ingredient}
+                readOnly={ingredient === 'flour'}
+              />
+            </div>
+          ))}
       </div>
       <div className="w-full border border-gray-400 md:w-1/2">
         <h2>Scaled</h2>
@@ -55,8 +86,7 @@ const Calculator = () => {
           <div key={`output-${ingredient}`}>
             <div>
               <p>
-                {ingredient} - {recipe.scaledIngredients(scale)[ingredient]}g
-                or {recipe.bakersPercentage(ingredient)}%
+                {ingredient} - {recipe.scaledIngredients(scale)[ingredient]}g or {recipe.bakersPercentage(ingredient)}%
               </p>
             </div>
           </div>
